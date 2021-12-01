2d2af5a2ad69c20d085e3571aa0f785c15995b13
Cody Frederick <cody.frederick@outlook.com>
2021-12-01 06:17:09 -0600

    checkpoint

--- js/net.js
+++ js/net.js
@@ -1,6 +1,7 @@
 const networking = {
   pubnub: null,
   start: function(uuid) {
+    console.log('networking started for', uuid);
     this.pubnub = new PubNub({
       publishKey: "pub-c-c20b0062-2fab-478d-b3a0-df24b3ad656f",
       subscribeKey: "sub-c-e4bd8c4a-3e23-11ec-b2c1-a25c7fcd9558",
@@ -8,15 +9,19 @@ const networking = {
     });
   },
   unsubscribe: function(...channels) {
+    console.log('unsubscribing from', channels);
     this.pubnub.unsubscribe({ channels });
   },
   subscribe: function(...channels) {
+    console.log('subscribing to', channels);
     this.pubnub.subscribe({ channels });
   },
   addListener: function(fn) {
+    console.log('adding listener');
     this.pubnub.addListener({
       status: e => console.log('status', e),
       message: e => {
+        console.log('raw message', e);
         if (e.publisher !== this.pubnub.getUUID()) {
           console.log('message', e);
           Reflect.apply(fn, null, e);
@@ -25,6 +30,7 @@ const networking = {
     });
   },
   publish: function(channel, message) {
+    console.log('publishing', channel, message);
     this.pubnub.publish({ channel, message }).catch(err => console.error(err));
   }
 };
@@ -41,6 +47,5 @@ const publishState = function(channel, state) {
       return acc;
     }, {})
   };
-  console.log('publishing', channel, message);
   networking.publish(channel, message);
 };
